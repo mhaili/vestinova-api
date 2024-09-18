fc647b44d6a35db524de039b49632da9ff8ffb63
cmoileo <leo.fezard33@gmail.com>
2024-09-18 19:37:52 +0200

    connect orm to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world !')
+import express from "express";
+import User from "./entities/User";
+import sequelize from "./sequelize.config";
+const initDb = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        await User.create({
+            name: "John Doe",
+            email: "johndoe@gmail.com",
+        });
+        const users = await User.findAll();
+        console.log(users)
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+
+initDb()
